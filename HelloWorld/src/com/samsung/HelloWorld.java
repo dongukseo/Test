f632b1e112ac4707c740b88df8604da8ecd7dcf2
dongukseo <dong-uk.seo@samsung.com>
2018-10-17 12:48:33 +0900

    Tesuuuuu

--- HelloWorld/src/com/samsung/HelloWorld.java
+++ HelloWorld/src/com/samsung/HelloWorld.java
@@ -2,7 +2,7 @@ package com.samsung;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Worl!!!d");
         System.out.println("Hello Linux on DeX !!");
     }
 }
